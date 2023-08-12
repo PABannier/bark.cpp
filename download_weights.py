bd26274b8d13ff3df7a098962c5f2c1d7cc6be97
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2023-08-12 20:24:52 +0200

    updated script

--- download_weights.py
+++ download_weights.py
@@ -5,7 +5,7 @@ from huggingface_hub import hf_hub_download
 import torch
 
 
-ENCODEC_PATH = "https://dl.fbaipublicfiles.com/encodec/v0/encodec_24khz-d7cc33bc.th"
+ENCODEC_PATH = Path("https://dl.fbaipublicfiles.com/encodec/v0/encodec_24khz-d7cc33bc.th")
 
 REMOTE_MODEL_PATHS = {
     "text": {
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     print(" ### Downloading EnCodec weights...")
     state_dict = torch.hub.load_state_dict_from_url(
-        ENCODEC_PATH,
+        str(ENCODEC_PATH),
         map_location="cpu",
         check_hash=True
     )
