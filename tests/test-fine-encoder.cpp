f5bda2e4948fbc42751359f386fe041d06c33926
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2023-08-16 12:26:23 +0200

    debug

--- tests/test-fine-encoder.cpp
+++ tests/test-fine-encoder.cpp
@@ -13,9 +13,10 @@ of tokens.
 #include "common.h"
 
 static const std::vector<std::string> test_data = {
-    // "./data/fine/test_fine_1.bin",
-    // "./data/fine/test_fine_2.bin"
-    "./data/fine/test_fine_3.bin"
+    "./data/fine/test_fine_1.bin",
+    "./data/fine/test_fine_2.bin",
+    "./data/debug_test.bin",
+    "./data/fine/long_prompt_nn_2.bin"
 };
 
 int main(int argc, char** argv) {
