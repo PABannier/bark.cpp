51d956a9d626422fa8b7c641f45b58322889d805
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2023-09-01 10:17:53 +0200

    test

--- tests/test-forward-fine.cpp
+++ tests/test-forward-fine.cpp
@@ -20,25 +20,30 @@ int main() {
 
     std::mt19937 rng(0);
 
-    gpt_model model;
-    if(!gpt_model_load(fname, model)) {
+    bark_model model;
+    if(!gpt_model_load(fname, model.fine_model)) {
         fprintf(stderr, "%s: invalid model file '%s'\n", __func__, fname.c_str());
         return 1;
     }
 
-    bark_codes input, gt_tokens, tokens;
+    bark_context * ctx = bark_new_context_with_model(&model);
+    if (ctx == nullptr) {
+        fprintf(stderr, "%s: failed to create context\n", __func__);
+        return 1;
+    }
+
+    bark_codes input, gt_tokens;
 
     for (int i = 0; i < (int) test_data.size(); i++) {
         input.clear();
         gt_tokens.clear();
-        tokens.clear();
 
         std::string path = test_data[i];
         load_test_data(path, input, gt_tokens);
 
         // TODO: need to remove transpose
         bark_codes input_t = transpose(input);
-        bark_codes tokens  = transpose(bark_forward_fine_encoder(input_t, model, rng, n_threads, temp));
+        bark_codes tokens = transpose(bark_forward_fine_encoder(*ctx, input_t, rng, n_threads, temp));
 
         printf("\n");
         printf("%s: %s\n", __func__, path.c_str());
