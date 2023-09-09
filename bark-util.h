4fddfb3484527c2450728521258a1763aa50ce18
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2023-09-10 00:24:26 +0200

    revert bark_util

--- bark-util.h
+++ bark-util.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <fstream>
-#include <vector>
 
 #define BARK_ASSERT(x) \
     do { \
@@ -28,16 +27,4 @@ static size_t utf8_len(char src) {
     const size_t lookup[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 4};
     uint8_t highbits = static_cast<uint8_t>(src) >> 4;
     return lookup[highbits];
-}
-
-template <typename E,typename X>
-void unroll(const std::vector<E>& v,std::vector<X>& out){
-    std::cout << "unroll vector\n";
-    out.insert(out.end(), v.begin(), v.end());
-}
-
-template <typename V,typename X>
-void unroll(const std::vector<std::vector<V>>& v,std::vector<X>& out) {
-    std::cout << "unroll vector of vectors\n";
-    for (const auto& e : v) unroll(e,out);
 }
