6642e75c914037c8cdb22f06aa40b2eb3ca62949
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2023-10-28 18:48:21 +0200

    remove codec parsing functions

--- convert.py
+++ convert.py
@@ -11,21 +11,11 @@ For each tensor, the bytes are packed as follows:
     - Name                    (char[name_length])
     - Data                    (float[n_dims])
 
-Note
-----
-Encodec uses weight normalization for its convolutional layers. All the weights are
-decomposed into two tensors called with the suffixes _weight_v and _weight_g. A simple
-call to the hook torch._weight_norm allows to get the final weight tensor of the
-convolution from weight_v and weight_g. To drastically reduce the number of operations
-at inference time, the ggml weights file only contain the final convolution weights but
-does not store the decomposition into weight_v and weight_g.
-
 Example
 -------
 ```bash
     python convert.py \
         --dir-model ~/.cache/suno/bark_v0 \
-        --codec-path ~/Documents/encodec.cpp/ggml_weights \
         --vocab-path ./ggml_weights/ \
         --out-dir ./ggml_weights/ \
         --use-f16
@@ -42,69 +32,10 @@ import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dir-model", type=str, required=True)
-parser.add_argument("--codec-path", type=str, required=True)
 parser.add_argument("--vocab-path", type=str, required=True)
 parser.add_argument("--out-dir", type=str, required=True)
 parser.add_argument("--use-f16", action="store_true")
 
-
-def parse_codec_model(checkpoint, out_dir):
-    """Load encodec model checkpoint."""
-    outfile = open(out_dir, "wb")
-    outfile.write(struct.pack("i", 0x67676d6c))  # ggml magic
-
-    for name in checkpoint.keys():
-        if "encoder." in name:
-            # bark only uses Encodec's quantizer and decoder.
-            continue
-
-        if "weight_g" in name:
-            # the tensor has already been parsed with the corresponding "weight_v"
-            # tensor to form the final weights tensor of the convolution, therefore
-            # we skip it
-            continue
-
-        if "inited" in name or "cluster_size" in name or "embed_avg" in name:
-            # "inited", "cluster_size" and "embed_avg" tensors in quantizer are not used
-            # for the forward pass
-            continue
-
-        var_data = checkpoint[name]
-
-        if not "weight_v" in name:
-            # if conv kernel, do not squeeze because 3d tensor
-            var_data = var_data.numpy().squeeze()
-        else:
-            # weight_v has its corresponding magnitude tensor to rescale the weights
-            # of the convolutional layers. We parse both kinds of weights jointly to
-            # build the final weight tensor of the convolution.
-            base_name = name.split(".")[:-1]
-            weight_g_name = ".".join(base_name + ["weight_g"])
-            var_data_g = checkpoint[weight_g_name]
-
-            final_var_data = torch._weight_norm(var_data, var_data_g, dim=0)
-            var_data = final_var_data.numpy()
-
-            name = ".".join(base_name + ["weight"])
-
-        print(f"Processing variable: {name} with shape: {var_data.shape}")
-
-        if var_data.dtype != np.float32:
-            print("  Converting to float32")
-            var_data = var_data.astype(np.float32)
-
-        n_dims = len(var_data.shape)
-        encoded_name = name.encode("utf-8")
-        ftype = 0  # float32
-        outfile.write(struct.pack("iii", n_dims, len(encoded_name), ftype))
-
-        for i in range(n_dims):
-            outfile.write(struct.pack("i", var_data.shape[n_dims - 1 - i]))
-        outfile.write(encoded_name)
-
-        var_data.tofile(outfile)
-
-    outfile.close()
 
 def parse_hparams(hparams, outfile, use_f16):
     """Parse GPT hyperparameters."""
@@ -127,7 +58,7 @@ def parse_hparams(hparams, outfile, use_f16):
         n_wtes = hparams["n_codes_total"]
     except KeyError:
         n_lm_heads, n_wtes = 1, 1
-    
+
     ftype = int(use_f16)
 
     outfile.write(struct.pack("iii", n_lm_heads, n_wtes, ftype))
@@ -139,12 +70,6 @@ def parse_text_models(checkpoint, outfile, use_f16):
         print(f"Processing variable: {name} with shape: {var_data.shape}")
 
         n_dims = len(var_data.shape)
-
-        # ftype_cur = 0
-        # if var_data.dtype != np.float32:
-        #     print("  Converting to float32")
-        #     var_data = var_data.astype(np.float32)
-        #     ftype_cur = 0
 
         # strip `_orig_mod.transformer.` prefix
         if name == "_orig_mod.lm_head.weight":
@@ -262,7 +187,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dir_model = Path(args.dir_model)
-    codec_path = Path(args.codec_path)
     vocab_path = Path(args.vocab_path)
 
     out_dir = Path(args.out_dir)
@@ -279,9 +203,5 @@ if __name__ == "__main__":
 
     generate_file(dir_model / "fine_2.pt", out_dir / "ggml_weights_fine.bin", args.use_f16)
     print(" Fine model loaded.")
-
-    codec_chkpt = torch.load(codec_path / "encodec_24khz-d7cc33bc.th", map_location="cpu")
-    parse_codec_model(codec_chkpt, out_dir / "ggml_weights_codec.bin")
-    print(" Codec model loaded.")
 
     print("Done.")
