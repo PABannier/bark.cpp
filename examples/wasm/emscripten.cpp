41b7adc186193337d135c70e981b5f6ad7627e4f
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-04-25 14:02:01 +0200

    cln

--- examples/wasm/emscripten.cpp
+++ examples/wasm/emscripten.cpp
@@ -89,9 +89,6 @@ EMSCRIPTEN_BINDINGS(bark) {
                          }));
 
     emscripten::function("getAudioBuffer", emscripten::optional_override([](size_t index) -> emscripten::val {
-                             printf("getAudioBuffer: index = %d\n", index);
-                             printf("context size = %d\n", g_contexts.size());
-                             printf("context ptr = %p\n", g_contexts[index]);
                              if (index >= g_contexts.size() || g_contexts[index] == nullptr) {
                                  return emscripten::val::null();
                              }
