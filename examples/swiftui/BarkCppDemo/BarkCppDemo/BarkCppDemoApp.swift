5307c58b01389d496af613a964393c457fdf4748
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-05-08 14:02:59 +0200

    swiftui : add Xcode demo project

---
+++ examples/swiftui/BarkCppDemo/BarkCppDemo/BarkCppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarkCppDemoApp.swift
+//  BarkCppDemo
+//
+//  Created by Pierre-Antoine BANNIER on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarkCppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ examples/swiftui/BarkCppDemo/BarkCppDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarkCppDemo
+//
+//  Created by Pierre-Antoine BANNIER on 08/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
