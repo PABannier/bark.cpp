3301e5dbdcfc9a09b871bf95fbf0a7a4203a85b5
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2024-05-08 16:06:14 +0200

    Updated app

--- examples/swiftui/BarkCppDemo/BarkCppDemo/ContentView.swift
+++ examples/swiftui/BarkCppDemo/BarkCppDemo/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var barkState = BarkState()
+    @State private var textInput: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                TextField("Enter your text here", text: $textInput)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity)
+                
+                HStack {
+                    Button("Generate Audio", action: {
+                        Task {
+                            await barkState.generateAudioFromText(prompt: textInput)
+                        }
+                    })
+                    .buttonStyle(.bordered)
+                    .disabled(!barkState.canGenerate || barkState.isGenerating)
+
+                    Button(barkState.isPlaying ? "Pause" : "Play", action: {
+                        if barkState.isPlaying {
+                            barkState.stopPlayback()
+                        } else {
+                            barkState.startPlayback()
+                        }
+                        barkState.isPlaying.toggle()
+                    })
+                    .buttonStyle(.bordered)
+                    .disabled(!barkState.canGenerate)
+                }
+                
+                ScrollView {
+                    Text(verbatim: barkState.messageLog)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+            .navigationTitle("Bark SwiftUI Demo")
+            .padding()
         }
-        .padding()
     }
 }
 
